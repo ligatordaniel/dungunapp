529d502d8675a2a7362ccbef408b4252b928836b
ligatorDaniel <danielulloa256@gmail.com>
2020-06-05 18:19:05 -0400

    agregar animacion btnhome

--- src/app/canales/cliente003/cliente003.component.ts
+++ src/app/canales/cliente003/cliente003.component.ts
@@ -9,7 +9,7 @@ export class Cliente003Component implements OnInit {
   showPlayButton = true;  /*para crear el toggle de play y stop*/
   showStopButton = false; /*para crear el toggle de play y stop*/
   /*url = 'https://radio.cloudserver.cl:7035/stream';*/   /*mi streming*/
-  /*url = 'https://radio.acktib.com:8443/radio.ogg';*/   /*otro streming*/
+  /*url = 'https://radio.acktib.com:8443/radio.ogg';*/   /*streming del tipo*/
   /*url = 'https://av03.upstream-media.ch/canal3francais-192.mp3';*/   /*radioTest*/
   url = 'https://av03.upstream-media.ch/canal3francais-192.mp3';   /*otro streming*/
 
--- src/app/canales/hellodemo/hellodemo.component.ts
+++ src/app/canales/hellodemo/hellodemo.component.ts
@@ -9,7 +9,7 @@ export class HellodemoComponent implements OnInit {
   showPlayButton = true;  /*para crear el toggle de play y stop*/
   showStopButton = false; /*para crear el toggle de play y stop*/
   /*url = 'https://radio.cloudserver.cl:7035/stream';*/   /*mi streming*/
-  /*url = 'https://radio.acktib.com:8443/radio.ogg';*/   /*otro streming*/
+  /*url = 'https://radio.acktib.com:8443/radio.ogg';*/   /*streming del tipo*/
   /*url = 'https://av03.upstream-media.ch/canal3francais-192.mp3';*/   /*radioTest*/
   url = '../../assets/media/audioPrueba1.mp3';   /*streaming actual*/
 
