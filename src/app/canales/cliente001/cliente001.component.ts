c30113c83834a5dd1892a6384163c708076bde4a
ligatorDaniel <danielulloa256@gmail.com>
2020-07-01 18:00:57 -0400

    agregada mi radio

--- src/app/canales/cliente001/cliente001.component.ts
+++ src/app/canales/cliente001/cliente001.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class Cliente001Component implements OnInit {
   showPlayButton = true;  /*para crear el toggle de play y stop*/
   showStopButton = false; /*para crear el toggle de play y stop*/
-  /*url = 'https://radio.cloudserver.cl:7035/stream';*/   /*mi streming*/
+  /*url = 'https://radio.cloudserver.cl/8076/dungun.ogg';*/   /*mi streming*/
   /*url = 'https://radio.acktib.com:8443/radio.ogg';*/   /*streming del tipo*/
   /*url = 'https://av03.upstream-media.ch/canal3francais-192.mp3';*/   /*radioTest*/
-  url = 'https://av03.upstream-media.ch/canal3francais-192.mp3';   /*streaming actual*/
+  url = 'https://radio.cloudserver.cl/8076/dungun.ogg';   /*streaming actual*/
 
   constructor() { }
 
--- src/app/canales/cliente002/cliente002.component.ts
+++ src/app/canales/cliente002/cliente002.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class Cliente002Component implements OnInit {
   showPlayButton = true;  /*para crear el toggle de play y stop*/
   showStopButton = false; /*para crear el toggle de play y stop*/
-  /*url = 'https://radio.cloudserver.cl:7035/stream';*/   /*mi streming*/
+  /*url = 'https://radio.cloudserver.cl/8076/dungun.ogg';*/   /*mi streming*/
   /*url = 'https://radio.acktib.com:8443/radio.ogg';*/   /*streming del tipo*/
   /*url = 'https://av03.upstream-media.ch/canal3francais-192.mp3';*/   /*radioTest*/
   url = 'https://radio.acktib.com:8443/radio.ogg';   /*streaming actual*/
